e567adffed383ce9f8e6786f36f5dc4dff9c8019
Xxvedant7xX <agrawalvedant2006@gmail.com>
2025-04-26 15:19:13 +0530

    created bottom nav bar,  shop and cart pages

---
+++ lib/models/shop.dart
@@ -0,0 +1,17 @@
+import 'package:bubble_tea_app/models/drink.dart';
+
+class BubbleTeaShop{
+
+  // list of drinks for sale
+
+
+  // list of drinks in user cart
+
+  // get drinks for sale
+
+  // get user cart
+
+  // add drink to cart
+
+  // remove drink from cart
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:bubble_tea_app/components/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
+
+import 'cart_page.dart';
+import 'shop_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +12,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // navigate bottom bar
+  int _selectedIndex = 0;
+  void navigateBottomBar(int newIndex) {
+    setState(() {
+      _selectedIndex = newIndex;
+    });
+  }
+
+  // pages to display
+  final List<Widget>_pages = [
+    // shoop page
+    ShopPage(),
+
+    // cart page
+    CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      bottomNavigationBar: MYBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
+    );
   }
 }
